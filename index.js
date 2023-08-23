0617697364b559f32375d5b3b2408d4099433298
Mardari Igor <igor.mardari96@gmail.com>
2023-08-23 22:31:47 +0300

    Update index.js
    Moved hard-coded URLs at the top of the file for a better visibility

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const integrationURL = ''; // Put here you slack webhook URL
+const integrationURL = ''; // Set your Slack Webhook URL
+
+const mainAmplifyAppURL = 'https://main.d3d9a90v2cc2tb.amplifyapp.com/';
+const realDomainAppURL = 'https://forum.useadrenaline.com';
+
+const amplifyBuildURLPattern = /Go to (https:\/\/console\.aws\.amazon\.com\/amplify\/home\?region=us-east-1#\w+\/main\/\d+)/
 
 function transformMessage(message) {
   // Remove the static portion
   let transformed = message.replace("Build notification from the AWS Amplify Console for app: ", "");
 
   // Replace initial URL
-  transformed = transformed.replace(/https:\/\/main\.d3d9a90v2cc2tb\.amplifyapp\.com\//, "https://youdomain.com");
+  transformed = transformed.replace(mainAmplifyAppURL, realDomainAppURL);
 
   // Capture and bold the build status
   transformed = transformed.replace(/Your build status is (\w+)\./, (match, status) => {
     return `\nYour build status is *${status}*.\n`;
   });
 
-  // Replace the long URL for build details with the hyperlink version
-  transformed = transformed.replace(/Go to (https:\/\/console\.aws\.amazon\.com\/amplify\/home\?region=us-east-1#\w+\/main\/\d+)/, "Go <$1|here>");
+  // Replace the long URL for build details with the shortened version
+  transformed = transformed.replace(amplifyBuildURLPattern, "Go <$1|here>");
 
   return transformed;
 }
